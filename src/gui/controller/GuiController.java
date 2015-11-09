c87a9942ec43400dbefcc5cc8d5774baef84f600
nibbles92 <droc6148@740s-pl-117.local>
2015-11-09 12:18:54 -0700

    Started new project and started biasframe

---
+++ src/gui/controller/GuiController.java
@@ -0,0 +1,23 @@
+package gui.controller;
+
+import gui.view.GUIFrame;
+
+/**
+ * Controller for the simple GUI project
+ * @author droc6148
+ *@version 0.1
+ */
+public class GuiController 
+{
+	private GUIFrame baseFrame;
+	
+	public GuiController()
+	{
+		baseFrame = new GUIFrame(this);
+	}
+	
+	public void start()
+	{
+		
+	}
+}
---
+++ src/gui/controller/GuiRunner.java
@@ -0,0 +1,20 @@
+package gui.controller;
+
+/**
+ * Launches the GUI Sample program
+ * @author droc6148
+ *@version 0.1 11/9/15
+ *
+ */
+public class GuiRunner 
+{
+	
+	/**
+	 * @param args Unused as this is a sample GUI
+	 */
+	public static void main(String [] args)
+	{
+		GuiController myController = new GuiController();
+		myController.start(); 
+	}
+}
